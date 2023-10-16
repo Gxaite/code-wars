ef90de56eca57884fa4885df8640941bdfb74010
Gabriel Scheidt <gabrielscheidt2000@gmail.com>
2023-10-16 14:17:00 -0300

    day 9 code wars

---
+++ 8-7 Kyu/shortest_word.py
@@ -0,0 +1,13 @@
+'''
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+'''
+def find_short(s):
+    list_string = s.split()
+    l = len(list_string[0])
+    for i in list_string:
+        if len(i)< l:
+            l = len(i)
+    return l
