daacd9fff50a29881f357ace88c05c9b8a14a58f
Gabriel Scheidt <gabrielscheidt2000@gmail.com>
2023-10-22 20:08:11 -0300

    Day 15 code wars

---
+++ 8-7 Kyu/spin.py
@@ -0,0 +1,33 @@
+'''
+Given a list of directions to spin, "left" or "right", return an integer of how many full 360° rotations were made. Note that each word in the array counts as a 90° rotation in that direction.
+Worked Example
+
+["right", "right", "right", "right", "left", "right"] ➞ 1
+# You spun right 4 times (90 * 4 = 360)
+# You spun left once (360 - 90 = 270)
+# But you spun right once more to make a full rotation (270 + 90 = 360)
+
+Examples
+
+["left", "right", "left", "right"] ➞ 0
+
+["right", "right", "right", "right", "right", "right", "right", "right"] ➞ 2
+
+["left", "left", "left", "left"] ➞ 1
+
+'''
+def spin_around(lst):
+    count_right = count_left = 0
+    for i in lst:
+        if i == "right":
+            count_right += 1
+        else: 
+            count_left += 1
+    
+    return abs(count_right * 90 -count_left * 90)//360
+            
+    
+    
+        
+
+
