7b2d7e0dbba47eb9fe52f8bbe92458abc0761ba2
Gabriel Scheidt <gabrielscheidt2000@gmail.com>
2023-11-13 13:12:30 -0300

    Day 26 code wars

---
+++ 8-7 Kyu/arithmetic_list.py
@@ -0,0 +1,21 @@
+'''
+In this kata, you will write an arithmetic list which is basically a list that contains consecutive terms in the sequence.
+You will be given three parameters :
+
+    first the first term in the sequence
+    c the constant that you are going to ADD ( since it is an arithmetic sequence...)
+    l the number of terms that should be returned
+
+Useful link: Sequence
+
+Be sure to check out my Arithmetic sequence Kata first ;)
+Don't forget about the indexing pitfall ;)
+
+
+'''
+def seqlist(first, c, l):
+    answ = [first]
+    for i in range(l-1):
+        answ.append(first+c)
+        first +=c
+    return answ
