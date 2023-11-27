a5966282f30ee53fa017dfa125ef0fb80ad7b7fe
Gabriel Scheidt <gabrielscheidt2000@gmail.com>
2023-11-26 21:11:39 -0300

    Day 31 code wars

---
+++ 8-7 Kyu/array_mult.py
@@ -0,0 +1,14 @@
+'''
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+Fundamentals
+Arrays
+'''
+def maps(a):
+    answ = []
+    for i in a:
+        answ.append(i*2)
+    return answ
